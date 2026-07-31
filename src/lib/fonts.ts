06b80e2c9be869d136aafb1ca13bd95053b4a346
KŌINI <support@koini.io>
2026-07-31 00:18:29 -0400

    Replace local font with Google font 'Inter_Tight'

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,12 +1,8 @@
-import localFont from 'next/font/local'
+import { Inter_Tight } from 'next/font/google'
 
-export const openSauceOne = localFont({
-  src: [
-    { path: '../../public/fonts/open-sauce-one-latin-400-normal.woff2', weight: '400', style: 'normal' },
-    { path: '../../public/fonts/open-sauce-one-latin-500-normal.woff2', weight: '500', style: 'normal' },
-    { path: '../../public/fonts/open-sauce-one-latin-600-normal.woff2', weight: '600', style: 'normal' },
-    { path: '../../public/fonts/open-sauce-one-latin-700-normal.woff2', weight: '700', style: 'normal' },
-  ],
+export const openSauceOne = Inter_Tight({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
   display: 'swap',
   variable: '--font-sans',
 })
